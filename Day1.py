b0894ba41bfc77f5b87e755705ecfc943db41196
ASofiaCastillo <angelitasofi190@gmail.com>
2024-10-17 19:51:34 -0500

    Day 1 #100-projects-in-100-days-Python

---
+++ Day1.py
@@ -0,0 +1,29 @@
+#print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl. \n" +
+#"2. Knead the dough for 10 minutes. \n" +
+#"3. Add 3g of Salt. \n" +
+#"4. Leave to rise for 2 hours. \n"+
+#"5. Bake at 200 degrees C for 30 minutes.") 
+
+# Fix the code below 👇
+
+#print("Notes from Day 1")
+#print("The print statement is used to output strings")
+#print("Strings are strings of characters")
+#print("String Concatenation is done with the + sign")
+#print("New lines can be created with a \ and the letter n")
+
+#print("Hello " + input("What's your name?") + "!")
+#username = input("what is your name?")
+#length = len(username)
+#print(length)
+
+#glass1 = "milk"
+#glass2 = "juice"
+#glass3 = glass2
+#glass2 = glass1
+#glass1 = glass3
+#print(glass1, glass2)
+print("Welcome to the band name generator")
+city_born = input("Name city where you born \n")
+name_pet= input("Name your pet \n")
+print("Your band name could be", city_born, name_pet)
